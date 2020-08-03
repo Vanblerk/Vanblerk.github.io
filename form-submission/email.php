ac130fb4ad2f0dd1aa3a872b12d59eced97afd56
vHavenga <vanblerk.havenga@advance.io>
2020-08-03 20:21:46 +0200

    SEO implementation

--- form-submission/email.php
+++ form-submission/email.php
@@ -49,8 +49,6 @@
 
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-//    $headers .= "From: " . $email . "\r\n";
-
     mail($to, $subject, $fullMessage, $headers);
 
     header('Location: '. 'form-submission-success.html');
